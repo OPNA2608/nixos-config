789b61b9bf26aea21d219e5276c99bd5f54ec82f
OPNA2608 <christoph.neidahl@gmail.com>
2021-08-07 23:12:06 +0200

    Fix gitignore

---
+++ machines/NixTop/configuration.nix
@@ -0,0 +1,107 @@
+{ config
+, pkgs
+, ...
+}:
+
+###
+### Configuration for my tower pc at home
+###
+
+let
+	gpuDriver = import ./profiles/graphics {
+		type = "amd";
+		useAlternative = false;
+	};
+	grub = import ./profiles/grub.nix {
+		supportEfi = true;
+	};
+	virtualisation = import ./profiles/virtualisation.nix {
+		vfioIds = [
+			# novideo GT 710
+			"10de:128b" "10de:0e0f"
+			# novideo GTX 650
+			"10de:0fc6" "10de:0e1b"
+		];
+	};
+in
+
+{
+	networking.hostName = "NixTop";
+	boot.kernelPackages = pkgs.linuxPackages_latest;
+
+	# NixOS compatibility version
+	system.stateVersion = "20.03";
+
+	# Build cores and jobs
+	nix.maxJobs    = 4;
+	nix.buildCores = 6;
+
+	imports = [
+		./hardware-configuration.nix
+
+		./profiles/common.nix
+		./profiles/desktop.nix
+		gpuDriver
+		grub
+		./profiles/devel.nix
+		./profiles/smart.nix
+		virtualisation
+
+		./users/bt1cn.nix
+	];
+
+	# Firewall
+	# networking.firewall.enable = true; # default
+
+	nixpkgs.config.allowUnfreePredicate = (pkg:
+		builtins.elem pkg.meta.description [
+			pkgs.corefonts.meta.description
+			pkgs.discord.meta.description
+			pkgs.input-fonts.meta.description
+			pkgs.minecraft.meta.description
+			pkgs.steam.meta.description
+			pkgs.steamPackages.steam-runtime.meta.description
+			pkgs.unrar.meta.description
+		]
+	);
+
+	# extra filesystems
+	fileSystems = let
+		fsOptions = [
+			"rw" "async" "nofail" "nouser" "errors=remount-ro"
+			"dmask=0003" "fmask=0113" "gid=${toString config.users.groups.users.gid}"
+		];
+	in {
+		"/mnt/perm/D1" = {
+			device = "/dev/disk/by-uuid/011621FA6BC2A92F";
+			fsType = "ntfs";
+			options = fsOptions;
+		};
+		"/mnt/perm/D2" = {
+			device = "/dev/disk/by-uuid/7F0015800BEEA225";
+			fsType = "ntfs";
+			options = fsOptions;
+		};
+		"/mnt/perm/Win10" = {
+			device = "/dev/disk/by-uuid/E09AF9689AF93C1A";
+			fsType = "ntfs";
+			options = fsOptions;
+		};
+	};
+
+	programs.steam = {
+		enable = true;
+		remotePlay.openFirewall = true;
+	};
+
+	# Extra packages
+	environment.systemPackages = with pkgs; [
+		(callPackage ./packages/gkraken.nix { })
+		wineWowPackages.full
+		# xboxdrv # don't think that's needed?
+	];
+
+	services.udev.packages = with pkgs; [
+		(callPackage ./packages/gkraken.nix { })
+	];
+}
