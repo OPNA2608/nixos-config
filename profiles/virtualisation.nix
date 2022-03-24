f7406d5c76b637472002c7882ab1a8c1b69c7ea0
OPNA2608 <christoph.neidahl@gmail.com>
2022-03-25 00:15:35 +0100

    profiles/virtualisation: fix for darwin audio

--- profiles/virtualisation.nix
+++ profiles/virtualisation.nix
@@ -14,12 +14,22 @@
 		'';
 		binfmt.emulatedSystems = [ "aarch64-linux" ];
 	};
+
 	virtualisation.libvirtd = {
 		enable = true;
 		onBoot = "ignore";
 		onShutdown = "shutdown";
-		qemu.package = pkgs.qemu_kvm;
+		qemu = {
+			package = pkgs.qemu_kvm;
+			runAsRoot = false;
+		};
 	};
+
+	# audio setup
+	hardware.pulseaudio.extraConfig = ''
+		load-module module-native-protocol-unix auth-group=qemu-libvirtd socket=/tmp/pulse-socket
+	'';
+
 	environment.systemPackages = with pkgs; [
 		virt-manager
 	];
