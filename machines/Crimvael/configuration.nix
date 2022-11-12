38a9b786dda3187daadca6b186967e13a06bc6b0
Puna2608 <opna2608@protonmail.com>
2022-11-12 23:12:12 +0100

    Crimvael: Add (Pinebook Pro)

---
+++ machines/Crimvael/configuration.nix
@@ -0,0 +1,60 @@
+{ config
+, pkgs
+, lib
+, ...
+}:
+
+###
+### Pine64 PineBook Pro config
+###
+
+let
+	grub = import ./profiles/grub.nix {
+		supportEfi = true;
+	};
+in
+
+{
+	networking.hostName = "Crimvael";
+	boot.kernelPackages = pkgs.linuxPackages_latest;
+
+	# NixOS compatibility version
+	system.stateVersion = "22.11";
+
+	nix.maxJobs = 2;
+	nix.buildCores = 2;
+
+	imports = [
+		./hardware-configuration.nix
+
+		<nixos-hardware/pine64/pinebook-pro>
+		#./nixos-hardware/pine64/pinebook-pro
+
+		./profiles/common.nix
+		./profiles/desktop.nix
+
+		grub
+		./profiles/devel.nix
+		./profiles/smart.nix
+
+		./users/puna.nix
+	];
+
+	nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+		"pinebookpro-ap6256-firmware"
+		"corefonts"
+		"input-fonts"
+	];
+
+	boot.loader.generic-extlinux-compatible.enable = false;
+	boot.loader.efi.canTouchEfiVariables = false;
+
+#	networking.networkmanager.extraConfig = ''
+#managed=false
+#'';
+
+	console.keyMap = "us";
+	services.xserver.layout = "us";
+	services.chrony.serverOption = "offline";
+}
+
