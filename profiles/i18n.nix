816f7c61a4bf9cefbe12d2ed113c62ee68f5f403
OPNA2608 <christoph.neidahl@gmail.com>
2023-01-08 14:46:24 +0100

    profiles/i18n: Re-enable Japanese locales
    Since https://github.com/NixOS/nixpkgs/commit/515b36c0939b2fd96072eef7e58722900218897b glibc is only built with a very
    minimal set of locales. This is me going along with the "only build locales you need" idea and figuring out what the
    intended way of doing that is.
    
    I hate this. I would REALLY prefer a i18n.extraSupportedLocales where I can just add the locales I need, rather than
    copy-pasting the default for i18n.supportedLocales here, just to add 2 more...

--- profiles/i18n.nix
+++ profiles/i18n.nix
@@ -3,9 +3,25 @@
 , ...
 }:
 
-{
+let
+	germanLocale = "de_DE.UTF-8";
+in {
 	console.keyMap = lib.mkDefault "de";
-	i18n.defaultLocale = "de_DE.UTF-8";
+	i18n = {
+		defaultLocale = germanLocale;
+		supportedLocales = builtins.map (l: l + "/" + (lib.lists.last (lib.strings.splitString "." l))) [
+			# This is kinda dumb, but we're gonna have to copy-paste the sane defaults here...
+			# Alternative is to put them in a random locale setting that gets sent into the systemd settings & the global
+			# envvars, which is equally silly. :/
+			"C.UTF-8"
+			"en_US.UTF-8"
+			germanLocale
+
+			# And now the additional ones
+			"ja_JP.UTF-8"
+			"ja_JP.EUC-JP"
+		];
+	};
 
 	services.xserver = let xEnabled = config.services.xserver.enable; in {
 		layout = lib.mkDefault (if xEnabled then "de" else null);
