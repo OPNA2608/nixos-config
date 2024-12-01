ed39e6751c81c80a60f911f8526683a3e55350ac
OPNA2608 <opna2608@protonmail.com>
2024-12-01 14:38:06 +0100

    packages/grundig-hw: Drop, unused

--- packages/grundig-hw.nix
+++
@@ -1,9 +0,0 @@
-{ writeTextDir
-}:
-
-writeTextDir "etc/udev/rules.d/60-grundig-hw.rules" ''
-  # Allow access to Grundig control hardware
-
-  # USB foot controller
-  SUBSYSTEMS=="usb", ATTRS{idVendor}=="15d8", ATTRS{idProduct}=="0024", MODE="0666"
-''
