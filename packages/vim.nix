bcbf45bf871f67c4cd6ed320c758b42ad2f69aed
OPNA2608 <opna2608@protonmail.com>
2025-10-26 11:39:11 +0100

    packages/vim: Move vimrc to separate file, try to enable SJIS support

--- packages/vim.nix
+++ packages/vim.nix
@@ -6,35 +6,7 @@ with import <nixpkgs> {
 vim_configurable.customize {
   name = "vim";
   vimrcConfig = {
-    customRC = ''
-      " Switch to Vim settings mode
-      set nocompatible
-
-      " Syntax highlighting
-      if has ("syntax")
-        syntax on
-      endif
-
-      " Cursor position
-      set ruler
-
-      " Disable mouse
-      set mouse-=a
-
-      " Line numbers
-      set number
-
-      " Tab -> 2 Spaces
-      set tabstop=2
-      set softtabstop=2
-      set shiftwidth=2
-      set noexpandtab
-
-      " statusline
-      set laststatus=2
-      " only show lightline's mode markers
-      set noshowmode
-    '';
+    customRC = builtins.readFile ./vimrc;
     packages.vim_plugins = with pkgs.vimPlugins; {
       start = [
         ale
