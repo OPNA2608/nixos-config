ad52e6828155caf0413237f824707fd6aad282ba
OPNA2608 <opna2608@protonmail.com>
2025-10-26 11:44:02 +0100

    Carlos: Updates
    - Drop Pantheon debugging stuff, seems more stable now
    - Override pantheon.elementary-dock to restore old instance launching behaviour
      I don't understand why they changed their defaults like that. Being unable to launch new instances of an app from the dock
      just because it doesn't specify some setting in its desktop file is *really* annoying
    - Drop minecraft unfree exception
      Package doesn't seem to exist anymore, and I don't have any interest in Minecraft anymore anyway...
    - Drop RCU override
      Everything's on the latest version again
    - Adjust defaultSession setting to Pantheon now being Wayland

--- machines/Carlos/base.nix
+++ machines/Carlos/base.nix
@@ -64,53 +64,18 @@ in {
 			};
 		})
 
-		# https://github.com/NixOS/nixpkgs/issues/274999 debugging
+		# ohmygod please make the dock just launch additional instances of applications again without any changes to every
+		# individual desktop file
 		(final: prev: {
-			gnome-session = prev.gnome-session.overrideAttrs (attrs: {
-				separateDebugInfo = true;
-			});
-			gnome-shell = prev.gnome-shell.overrideAttrs (attrs: {
-				separateDebugInfo = true;
-			});
-			mutter = prev.mutter.overrideAttrs (attrs: {
-				separateDebugInfo = true;
-			});
-			mutter43 = prev.mutter43.overrideAttrs (attrs: {
-				separateDebugInfo = true;
-			});
-
-			# Attempting to fix this with a patch
 			pantheon = prev.pantheon.overrideScope (gfinal: gprev: {
-				gala = gprev.gala.overrideAttrs (oa: {
-					patches = (oa.patches or []) ++ [
-						(prev.fetchpatch {
-							name = "9991-gala-More-checks-in-notifications-stack.patch";
-							url = "https://github.com/elementary/gala/commit/186e9a304a02bcef01a97a4336438c2969c57754.patch";
-							hash = "sha256-aC0MH54InRj4MK7nxlEyAZxzsPh338V5P/gFuGkTfDc=";
-						})
-/*
-						(prev.fetchpatch {
-							name = "9992-gala-PanelWindow-Fix-possible-segfault.patch";
-							url = "https://github.com/elementary/gala/commit/fdd5e440d43af570e32710d026164fc0683752c9.patch";
-							hash = "sha256-fefOkkqGX6mo/NzykDdkTDz7oZha3jxzfBQ8piIGn9E=";
-						})
-						(prev.fetchpatch {
-							name = "9993-gala-ShellClients-Fix-crash-when-positioning-window-while-unmanaging.patch";
-							url = "https://github.com/elementary/gala/commit/48b716337f51a8d1c798015c8d677d2d5fa36e4d.patch";
-							hash = "sha256-KVRG2C4i11L5o0ZpSoGsDHEqs2jjdPaXup2u9PYdHfo=";
-						})
-*/
-
-						# https://github.com/elementary/gala/pull/2129
-            /*
-						(prev.fetchpatch {
-							name = "9994-gala-WindowManager-Only-show-notifications-after-their-window-was-shown.patch";
-							url = "https://github.com/elementary/gala/commit/719c2dbfe1975bc307892ddeb4e51fd9b8b784d4.patch";
-							hash = "sha256-bTIXn8SLJwM5N2rwjSb9iDNwguvU+qLZWT4JV28+zrU=";
-						})
-            */
-						../../packages/gala-0001-WindowManager-Only-show-notifications-after-their-wi.patch
-					];
+				elementary-dock = gprev.elementary-dock.overrideAttrs (oa: {
+					# Treat undefined SingleMainWindow as set to false, to launching new instance of app can just be attempted
+					postPatch = ''
+						substituteInPlace src/AppSystem/App.vala \
+							--replace-fail \
+								'app_info.get_string ("SingleMainWindow")' \
+								'app_info.get_boolean ("SingleMainWindow") ? "true" : "false"'
+					'';
 				});
 			});
 		})
@@ -130,7 +95,6 @@ in {
 			pkgs.corefonts.meta.description
 			pkgs.discord.meta.description
 			pkgs.input-fonts.meta.description
-			pkgs.minecraft.meta.description
 			pkgs.steam-unwrapped.meta.description
 			pkgs.steam.meta.description
 			pkgs.steam.run.meta.description
@@ -186,72 +150,12 @@ in {
 
 		protonmail-bridge
 
-		# Connecting to my tablet
-		# Older versions are harder to get
-		(rcu.overrideAttrs (oa: rec {
-			version = "2025.001s";
-			src =
-				let
-					src-tarball = requireFile {
-						name = "rcu-d${version}-source.tar.gz";
-						hash = "sha256-QC9ieulYAmE9pwt1/eZmyI5MZfRV0f24Pe5oKtuXNok=";
-						url = "https://www.davisr.me/projects/rcu/";
-					};
-				in
-				runCommand "${src-tarball.name}-unpacked" { } ''
-					gunzip -ck ${src-tarball} | tar -xvf-
-					mv rcu $out
-					ln -s ${src-tarball} $out/src
-				'';
-
-				# Update refs to older src
-				installPhase =
-					''
-						runHook preInstall
-
-						mkdir -p $out/{bin,share}
-						cp -r src $out/share/rcu
-
-					''
-					+ lib.optionalString stdenv.hostPlatform.isLinux ''
-						install -Dm644 package_support/gnulinux/50-remarkable.rules $out/etc/udev/rules.d/50-remarkable.rules
-					''
-					+ ''
-
-						# Keep source from being GC'd by linking into it
-
-						for icondir in $(find icons -type d -name '[0-9]*x[0-9]*'); do
-							iconsize=$(basename $icondir)
-							mkdir -p $out/share/icons/hicolor/$iconsize/apps
-							ln -s ${src}/icons/$iconsize/rcu-icon-$iconsize.png $out/share/icons/hicolor/$iconsize/apps/rcu.png
-						done
-
-						mkdir -p $out/share/icons/hicolor/scalable/apps
-						ln -s ${src}/icons/64x64/rcu-icon-64x64.svg $out/share/icons/hicolor/scalable/apps/rcu.svg
-
-						mkdir -p $out/share/doc/rcu
-						for docfile in {COPYING,manual.pdf}; do
-							ln -s ${src}/manual/$docfile $out/share/doc/rcu/$docfile
-						done
-
-						mkdir -p $out/share/licenses/rcu
-						ln -s ${src}/COPYING $out/share/licenses/rcu/COPYING
-
-						runHook postInstall
-					'';
-		}))
+		rcu
 
 		# For Lomiri upstream submissions
 		# Clickable needs to be latest one
 		nixpkgs-unstable.clickable
 		xorg.xhost
-
-		# https://github.com/NixOS/nixpkgs/issues/274999 debugging
-		glib
-		gnome-session
-		mutter
-		mutter43
-		gnome-shell
 	];
 
 	# https://github.com/NixOS/nixpkgs/issues/274999 debugging
@@ -264,7 +168,7 @@ in {
 	programs.haguichi.enable = true;
 
 	services.desktopManager.lomiri.enable = true;
-	services.displayManager.defaultSession = lib.mkForce "pantheon";
+	services.displayManager.defaultSession = lib.mkForce "pantheon-wayland";
 	services.xserver.displayManager.lightdm.greeters = {
 		pantheon.enable = lib.mkForce false;
 		lomiri.enable = lib.mkForce true;
